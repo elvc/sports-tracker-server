14e241a0f1f346010cfe641dd0a2053c31336e66
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-29 15:16:04 -0700

    Feature/user cards (#23)
    * Added feature; store user cards
    
    * Print errors

--- routes/game_api.js
+++ routes/game_api.js
@@ -5,6 +5,7 @@ const api_router = express.Router();
 const ENV = process.env.NODE_ENV || 'development';
 const knexConfig = require('../knexfile');
 const knex = require('knex')(knexConfig[ENV]);
+const dbCards = require('../db/cards')(knex);
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const moment = require('moment-timezone');
@@ -18,31 +19,10 @@ const config = {
   }
 };
 
-const api_key = process.env.GOOGLE_API_KEY;
 const date_format = 'YYYYMMDD';
 const date_time_zone = 'America/Los_Angeles';
 const game_time_zone = 'America/New_York';
 
-
-function getStartingTime(game){
-  const date = game.date;
-  const time = game.time;
-  const city = game.homeTeam.City;
-  const location = game.location;
-  return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}+${city}&sensor=false&key=${api_key}`)
-  .then(json => {
-    const result = json.data.results[0];
-    const latitude = result.geometry.location.lat;
-    const longitude = result.geometry.location.lng;
-    const location = `${latitude},${longitude}`;
-    return axios.get(`https://maps.googleapis.com/maps/api/timezone/json?location=${location}&timestamp=1331161200&sensor=false&key=${api_key}`)
-    .then(json => {
-      const timezone = json.data.timeZoneId;
-      const startTime = moment.tz(`${date} ${time}`, "YYYY-MM-DD hh:mmA", game_time_zone);
-      return startTime.tz('America/Los_Angeles');
-    })
-  });
-};
 
 module.exports = (function() {
 
@@ -55,25 +35,33 @@ module.exports = (function() {
         res.json({ response: {}});
         return;
       }
-      Promise.all(json.data.dailygameschedule.gameentry.map(game => {
-        return getStartingTime(game)
-          .then(newtime => {
-            game.time = newtime.format('hh:mmA');
-            game.gameId = game.id;
-            return game;
-          });
-      })).then(values => {
-        res.json({ response: values })
-      });
+      let startTime;
+      const values = json.data.dailygameschedule.gameentry.map(game => {
+        game.gameId = game.id;
+        startTime = moment.tz(`${game.date} ${game.time}`, "YYYY-MM-DD hh:mmA", game_time_zone);
+        game.time =  startTime.tz(date_time_zone).format('hh:mmA');
+        return game;
+      })
+      res.json({ response: values });
     }).catch(error => {
-        console.log(error);
+        console.log(error.response.data.message);
         // TODO what to do with errors!!!!!!!!!!!!!!1
         res.json({ response: {}})
     });
   });
 
   api_router.post('/:league/games/:id', (req, res) => {
-    addCard(req.body, res);
+    const user_id = req.session.user_id;
+    addCard(user_id, req.body, res);
+  });
+
+  api_router.get('/:league/users/:id', (req, res) => {
+    const user_id = req.session.user_id;
+    if(user_id){
+      dbCards.getCardsByUser(user_id).then(result => {
+        res.json({response: result});
+      })
+    }
   });
 
   return api_router;
