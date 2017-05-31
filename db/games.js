c1e94f5be34c8587c072b7253b90add9c086270f
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-30 18:09:23 -0700

    implemented caching of most current games in db and changed db schema (#28)

---
+++ db/games.js
@@ -0,0 +1,19 @@
+module.exports = function(knex) {
+  return {
+    findByLeagueAndDate: (league, date) => {
+      return knex.select('games.*', 'awayteam.abbreviation AS awayteam', 'hometeam.abbreviation AS hometeam')
+      .from('games')
+      .join('teams AS awayteam', 'games.away_team_id', 'awayteam.id')
+      .join('teams AS hometeam', 'games.home_team_id', 'hometeam.id')
+      .where('games.league', '=', league).andWhere('games.date', '=', date);
+    },
+    findGame: (id) => {
+      return knex.select('*')
+      .from('games')
+      .where('id','=', id);
+    },
+    insertGame: (game) => {
+      return knex('games').insert({id: game.gameId, league: game.league, awayTeam: game.awayTeam, homeTeam: game.homeTeam, time: game.time, date: game.date});
+    }
+  };
+};
